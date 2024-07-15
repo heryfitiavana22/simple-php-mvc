e7196e03426074e7f242f6ad53e6e82a0f647c82
heryfitiavana22 <hheryfitiavana@gmail.com>
2024-07-15 12:09:34 +0300

    feat: support sqlite and auto create file sqlite

--- config/database.php
+++ config/database.php
@@ -1,22 +1,13 @@
 <?php
 
+use Helpers\DbConnection;
 use Illuminate\Database\Capsule\Manager as Capsule;
 use Illuminate\Events\Dispatcher;
 use Illuminate\Container\Container;
 
 $capsule = new Capsule;
 
-$capsule->addConnection([
-    'driver'    => $_ENV['DB_DRIVER'],
-    'host'      => $_ENV['DB_HOST'],
-    'database'  => $_ENV['DB_DATABASE'],
-    'username'  => $_ENV['DB_USERNAME'],
-    'password'  => $_ENV['DB_PASSWORD'],
-    'charset'   => $_ENV['DB_CHARSET'],
-    'collation' => $_ENV['DB_COLLATION'],
-    'prefix'    => $_ENV['DB_PREFIX'],
-]);
-
+$capsule->addConnection(DbConnection::get());
 
 $capsule->setAsGlobal();
 $capsule->bootEloquent();
